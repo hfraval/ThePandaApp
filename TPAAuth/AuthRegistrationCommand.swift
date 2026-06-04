9240370bd5be206bbbb69256f8a4afc507490ff2
Herve Fraval <hfl_creation@hotmail.com>
2026-06-04 14:09:40 +0200

    make the protocol a registerInstance (most other dI should be like that as well)

--- TPAAuth/AuthRegistrationCommand.swift
+++ TPAAuth/AuthRegistrationCommand.swift
@@ -6,6 +6,6 @@ public struct AuthRegistrationCommand: ServiceContainerRegistrationCommand {
     public func execute(for container: ServiceContainer) {
         container.registerSingleton(as: LoginServiceProtocol.self) { LoginService() }
         container.registerSingleton(as: LoginActionProtocol.self) { LoginAction() }
-        container.registerSingleton(as: LoginViewModelProviderProtocol.self) { LoginViewModelProvider() }
+        container.registerInstance(LoginViewModelProvider(), as: LoginViewModelProviderProtocol.self)
     }
 }
